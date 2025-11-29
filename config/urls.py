3ed80ddc746266c2cab7fe15768089dc8c453482
too0oori <sofia.lagos.cesped@gmail.com>
2025-11-28 22:31:02 -0300

    se arregla home

--- config/urls.py
+++ config/urls.py
@@ -4,14 +4,16 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
+from apps.productos import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', RedirectView.as_view(url='/productos/', permanent=False)),
+    path('', views.HomeView.as_view(), name='home'),
     path('usuarios/', include('apps.usuarios.urls')),
     path('productos/', include('apps.productos.urls')),
     path('pedidos/', include('apps.pedidos.urls')),
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
